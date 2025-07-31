e85827b37e7dd33cbeda6cb42a7c79d01d9a038a
Kerolayne <karolgrassi.d@hotmail.com>
2025-07-30 22:11:14 -0300

    aprimorando a estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
   );
 
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-      <div className="col-span-3 max-w-sm">
+    <main className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-5xl mx-auto">
+      <div className="col-span-3 max-w-sm mt-2">
         <div className="col-span-1">
           <div className="relative flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
@@ -36,7 +36,7 @@ export default function Home() {
             </svg>
         
             <input
-            className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            className="bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
             placeholder="Name or e-mail" 
             value={search}
             onChange={(ev) => setSearch(ev.target.value)} 
