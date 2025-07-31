474b42e4de63958175e1a52358a46efdc569eca1
Kerolayne <karolgrassi.d@hotmail.com>
2025-07-30 21:52:42 -0300

    habilitando filtro de busca por letra digitada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('')
+  
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((json) => {
-        setData(json);
+        setUsers(json);
         setLoading(false);
       })
       .catch((err) => {
@@ -19,9 +21,39 @@ export default function Home() {
       });
   }, []);
 
+  const filteredUsers = users.filter((user) => 
+    user.name.toLowerCase().includes(search.toLowerCase()) ||
+    user.email.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-      {data.map((user) => (
+      <div className="col-span-3 max-w-sm">
+        <div className="col-span-1">
+          <div className="relative flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
+              <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+            </svg>
+        
+            <input
+            className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            placeholder="Name or e-mail" 
+            value={search}
+            onChange={(ev) => setSearch(ev.target.value)} 
+            />
+            
+            <button
+              className="rounded-md bg-slate-800 py-2 px-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+              type="button"
+            >
+              Search
+            </button> 
+          </div>         
+        </div>
+
+      </div>
+
+      {filteredUsers.map((user) => (
         <div className="bg-white shadow-sm border border-slate-200 rounded-lg">
           <div className="p-4">
             <h5 className="mb-2 text-slate-800 text-xl font-semibold">
